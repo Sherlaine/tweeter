9efeee5e1b8bc25ebc5f13e8f63eeab7c2dbd2ad
Sherlaine <sherlaine.lau@gmail.com>
2018-10-10 02:54:40 +0000

    fixed counter to red and black, and changed counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     console.log("document ready!");
     $('.new-tweet textarea').on('keypress', function () {
-        let charCounter = 140;
-        let charTyped = $(this).val().length;
-        let charLeft = charCounter - charTyped;
+        let charLeft = 140 - $(this).val().length;
         console.log(charLeft)
-
-        $('.counter').text(charLeft)
+        $('#counter').text(charLeft)
+        if (charLeft < 0) {
+            $('#counter').css('color', 'red');
+        } else {
+            $('#counter').css('color', 'black');
+        }
     });
 });
