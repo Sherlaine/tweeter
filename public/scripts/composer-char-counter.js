b44cf82c600181248edaa4b9e43d3a95e60acd1b
Sherlaine <sherlaine.lau@gmail.com>
2018-10-10 02:40:58 +0000

    added counter to text

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,11 @@
-console.log("composer-char-counter added!");
+$(document).ready(function () {
+    console.log("document ready!");
+    $('.new-tweet textarea').on('keypress', function () {
+        let charCounter = 140;
+        let charTyped = $(this).val().length;
+        let charLeft = charCounter - charTyped;
+        console.log(charLeft)
+
+        $('.counter').text(charLeft)
+    });
+});
