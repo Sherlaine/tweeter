c2847ff735b29046135bd27e8f72529362a0dac5
Sherlaine <sherlaine.lau@gmail.com>
2018-10-13 18:05:37 +0000

    added favicons and style changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,28 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(function () {
 
     function createTweetElement(tweetData) {
+        //header's children and append after
         let $h1 = $("<h1>").addClass("username").text(tweetData.user.handle);
         let $h2 = $(`<h2 class="fullname">`).text(tweetData.user.name)
         let $img = $("<img>").addClass("avatar").attr("src", tweetData.user.avatars.small);
-
         let $header = $("<header>").append($img, $h1, $h2);
 
         let $p = $("<p>").text(tweetData.content.text)
         let $div = $("<div>").addClass("tweet-content").append($p)
 
+        // icons and timestamp append to footer 
+        let $icons = $('<div class="icons">');
+        let $iFlag = $('<i class="fa fa-flag">');
+        let $iRetweet = $('<i class="fa fa-retweet">');
+        let $iHeart = $('<i class="fa fa-heart like-button">');
+        $icons.append($iFlag, $iRetweet, $iHeart);
         let $p2 = $("<p>").addClass("timestamp").text(tweetData.created_at)
-        let $footer = $("<footer>").append($p2)
+        let $footer = $("<footer>").append($icons, $p2)
 
         //tweet variable now appends all its children 
         let $tweet = $("<article>")
             .addClass("tweet")
-            .append($header)
-            .append($div)
-            .append($footer)
+            .append($header, $div, $footer);
 
         return $tweet;
     }
@@ -37,12 +36,10 @@ $(document).ready(function () {
 
     }
 
-    // renderTweets(data);
-
-
     function showMessage(message) {
         alert(message);
     };
+
 
     $('#createTweet').on('submit', function (event) {
         event.preventDefault();
